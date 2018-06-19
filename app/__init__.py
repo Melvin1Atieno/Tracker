06b8d76e03fff723ef6ed3567038aa30fb5db562
naibor <liznaibor.en@gmail.com>
2018-06-19 19:46:47 +0300

    [Feature]Add user registration route

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,27 @@
 from flask_api import FlaskAPI
 
-from flask import jsonify
+from flask import jsonify, request, make_response
 
 from config import app_config
 
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from models import User
 
 
+
+users_catalog = [
+    {
+        "username": "melvin",
+        "email": "mel@example.com",
+        "password":"12345"
+    },
+    {
+        "username": "mel",
+        "email": "me@example.com",
+        "password": "123"
+    }
+]
 
 
 
@@ -14,4 +30,43 @@ def create_app(config_name):
     app.config.from_object(app_config["development"])
     app.config.from_envvar("APP_SETTINGS")
 
+
+
+        # import pdb; pdb.set_trace()
+    @app.route("/api/v1/users", methods=["POST"])
+    def create_user():
+        data = request.get_json()
+        hashed_password = generate_password_hash(data.get("password"), method="sha256")
+
+
+        if not data.get("username"):
+            return make_response(jsonify({"message": "Username not provided"}),400)
+        if not data.get("email"):
+            return make_response(jsonify({"message": "Email not provided"}),400)
+        if not data.get("password"):
+            return make_response(jsonify({"message": "Password not provided"}),400)
+        if data.get("username") == " ":
+            return make_response(jsonify({"message": "Username field cannot be empty"}),400)
+        if data.get("email") == " ":
+            return make_response(jsonify({"message": "Email field cannot be empty"}),400)
+        if data.get("password") == " ":
+            return make_response(jsonify({"message": "Password field cannot be empty"}),400)
+        if "@" and ".com" not in data.get("email"):
+            return make_response(jsonify({"message":"Invalid email"}),400)
+
+
+        username = data.get("username")
+        email = data.get("email")
+        password = hashed_password
+        for user in users_catalog:
+            if user["username"] == username:
+                return make_response(jsonify({"message": "Username already taken"}),400)
+            if user["email"] == email:
+                return make_response(jsonify({"message": "Email already registered to a user"}),400)
+        
+        new_user = User(username,email,password)
+        users_catalog.append({"username":new_user.username,"email":new_user.email,"password":new_user.password})
+        return make_response(jsonify({"message": "Registration Successful"}),201)
+        
+        
     return app
--- tests/test-registration.py
+++ tests/test-registration.py
@@ -1,7 +1,7 @@
 import unittest
 import os
 import json
-from app import app
+from app import create_app
 from flask import Flask
 
 
@@ -9,8 +9,7 @@ class UserRegistrationTestCase(unittest.TestCase):
     """Test cases for user registration"""
 
     def setUp(self):
-        self.app = Flask(__name__)
-        self.app.config['TESTING'] = True
+        self.app = create_app("testing")
         self.client = self.app.test_client()
         self.data = {
             "username": "melvin",
@@ -18,40 +17,18 @@ class UserRegistrationTestCase(unittest.TestCase):
             "password": "12345"
         }
 
-    def test_new_user_successful_registeration_status_code(self):
+    def test_new_user_registeration(self):
         """Test that a user can register if they provide details"""
-        register = self.client.post("api/v1/users/", data=json.dumps(self.data),
-                                    content_type=("application/json")
-                                    )
+        register = self.client.post("api/v1/users", data=json.dumps(dict(username="newuser", email="newuser@gmail.com", password="1234")),
+                                    content_type=("application/json"))
         self.assertEqual(register.status_code, 201)
         response = json.loads(register.data.decode())
         self.assertEqual(response["message"], "Registration Successful")
-
-
-    def test_existing_user_registration_status_code(self):
-        """Test the status code for registration of existing user"""
-        register = self.client.post("api/v1/users/", data=json.dumps(self.data),
-                                    content_type=("application/json")
-                                    )
-        register2 = self.client.post("api/v1/users/", data=json.dumps(self.data),
-                                     content_type=("application/json")
-                                     )
-        self.assertEqual(register2.status_code, 400)
-
-    def test_existing_user_registration_response_message(self):
-        """Test for appropriate response message for registration of existing user"""
-        register = self.client.post("api/v1/users/", data=json.dumps(self.data),
-                                    content_type=("application/json")
-                                    )
-        register_two = self.client.post("api/v1/users/", data=json.dumps(self.data),
-                                        content_type=("application/json")
-                                        )
-        response = json.loads(register_two.data.decode())
-        self.assertEqual(response["message"], "User already exists")
+                                     
 
     def test_user_registration_without_username(self):
         """Test for appropriate response message for registration without username"""
-        register = self.client.post("api/v1/users/", data=json.dumps(dict(email="example@gmail", password="12345")),
+        register = self.client.post("api/v1/users", data=json.dumps(dict(email="person@gmail", password="12345")),
                                     content_type=("application/json")
                                     )
         response = json.loads(register.data.decode())
@@ -60,66 +37,67 @@ class UserRegistrationTestCase(unittest.TestCase):
 
     def test_user_registration_with_empty_string_username(self):
         """Test for appropriate response message for registration with empty string username"""
-        register = self.client.post("api/v1/users/", data=json.dumps(dict(username=" ", email="example@gmail", password="1234")),
+        register = self.client.post("api/v1/users", data=json.dumps(dict(username=" ", email="example@gmail", password="1234")),
                                     content_type=("application/json"))
         response = json.loads(register.data.decode())
-        self.assertEqual(response["message"], "Username cannot be empty")
+        self.assertEqual(response["message"], "Username field cannot be empty")
         self.assertEqual(register.status_code,400)
 
     def test_user_registration_without_email(self):
         """Test reponse message for user registration without email"""
-        register = self.client.post("api/v1/users/", data=json.dumps(dict(username="example", password="1234")),
+        register = self.client.post("api/v1/users", data=json.dumps(dict(username="example", password="1234")),
                                     content_type=("application/json"))
         response = json.loads(register.data.decode())
-        self.assertEqual(response["message"], "Email must be provided")
+        self.assertEqual(response["message"], "Email not provided")
         self.assertEqual(register.status_code, 400)
 
     def test_user_registration_with_empty_string_email(self):
         """Test for appropriate response message for empty string email registration"""
-        register = self.client.post("api/v1/users/", data=json.dumps(dict(username="example", email=" ", password="12345")),
+        register = self.client.post("api/v1/users", data=json.dumps(dict(username="example", email=" ", password="12345")),
                                     content_type=("application/json"))
         response = json.loads(register.data.decode())
         self.assertEqual(response["message"], "Email field cannot be empty")
 
     def test_user_registration_without_password(self):
         """Test for appropriate response message for registration without password"""
-        register = self.client.post("api/v1/users/", data=json.dumps(dict(username="example", email="example@gmail")),
+        register = self.client.post("api/v1/users", data=json.dumps(dict(username="another", email="another@gmail")),
                                     content_type=("application/json"))
         response = json.loads(register.data.decode())
-        self.assertEqual(response["message"], "Password must be provided")
+        self.assertEqual(response["message"], "Password not provided")
         self.assertEqual(register.status_code, 400)
 
     def test_user_registration_with_empty_string_password(self):
         """Test for approprate response for user registration with empty string password"""
-        register = self.client.post("api/v1/users/", data=json.dumps(dict(username="example", email="example@gmail", password=" ")),
+        register = self.client.post("api/v1/users", data=json.dumps(dict(username="example", email="example@gmail", password=" ")),
                                     content_type=("application/json"))
         response = json.loads(register.data.decode())
         self.assertEqual(response["message"], "Password field cannot be empty")
         self.assertEqual(register.status_code, 400)
 
     def test_user_registration_with_existing_username(self):
-        """Test for user registration with already existing email"""
-        register = self.client.post("api/v1/users/", data=json.dumps(self.data),
+        """Test for user registration with already existing username"""
+        register = self.client.post("api/v1/users", data=json.dumps(self.data),
                                     content_type=("application/json"))
-        register_two = self.client.post("api/v1/users/", data=json.dumps(dict(username="melvin", email="differnt@gmail", password="1234")),
+        register_two = self.client.post("api/v1/users", data=json.dumps(dict(username="melvin", email="differnt@gmail.com", password="1234")),
                                         content_type=("application/json"))
         response = json.loads(register_two.data.decode())
-        self.assertEqual(response["message"], "Username already exists")
+        self.assertEqual(response["message"], "Username already taken")
         self.assertEqual(register_two.status_code, 400)
 
     def test_user_registration_with_existing_email(self):
         """Test response message for registration with already existing email"""
-        register = self.client.post("api/v1/users/", data=json.dumps(self.data),
+        register = self.client.post("api/v1/users", data=json.dumps(dict(username="name", email="email@gmail.com", password="1234")),
                                     content_type=("application/json"))
-        register_two = self.client.post("api/v1/users/", data=json.dumps(dict(username="different name", email="email@gmail.com", password="1234")),
+        register_two = self.client.post("api/v1/users", data=json.dumps(dict(username="different name", email="email@gmail.com", password="1234")),
                                         content_type=("application/json"))
         response = json.loads(register_two.data.decode())
-        self.assertEqual(response["message"], "Email already exists")
+        self.assertEqual(response["message"],
+                         "Email already registered to a user")
         self.assertEqual(register_two.status_code, 400)
 
     def test_user_registration_with_invalid_email(self):
         """test response message for registration with invalid email"""
-        register = self.client.post("api/v1/users/", data=json.dumps(dict(username="name", email="email", password="1234")),
+        register = self.client.post("api/v1/users", data=json.dumps(dict(username="name", email="email", password="1234")),
                                     content_type=("application/json"))
         response = json.loads(register.data.decode())
         self.assertEqual(response["message"], "Invalid email")
